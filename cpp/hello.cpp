4b6f6e388d5410536d19fbe839ae8099658b1578
Abhinavmanda1857 <155139627+Abhinavmanda1857@users.noreply.github.com>
2023-12-29 07:27:54 +0000

    my first program

---
+++ cpp/hello.cpp
@@ -0,0 +1,34 @@
+// #include<cstdio>
+// using namespace std;
+// int main(){
+//     puts("hi world");
+//     return 0;
+// }
+// void nohing(){
+//     puts("hello");
+// }
+// char iamachar(){
+//     return 'h';
+// }
+
+
+
+
+//L-2
+// #include <iostream>
+// using namespace std;//so no need to use std:: everytime
+// int main(){
+//     cout<<"hello"<<endl;
+//     return 0;
+// //anyting after return is neglected
+// }
+
+//L-3
+#include <iostream>
+using namespace std;
+int main(){
+    int no;
+    cin>>no;
+    printf("id is %d\n",no+3);
+    return 0;
+}
