440d630a39d29ef1fc663ccf4c44c114f1bb448f
Abhinav Manda <155139627+Abhinavmanda1857@users.noreply.github.com>
2025-01-16 16:10:37 +0000

    2425. Bitwise XOR of All Pairings

---
+++ cpp/lc16/rr.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int xorAllNums(vector<int>& nums1, vector<int>& nums2) {
+    int n = nums1.size(),m = nums2.size(),xr1 = 0,xr2 = 0,ans = 0;
+    for(auto it:nums2) xr2^=it;
+    for(auto it:nums1)
+    xr1 = (m%2==0)?0:it,ans ^= (xr1^xr2);
+    return ans;    
+    }
+};
