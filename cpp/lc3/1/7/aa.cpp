14a7513f023355b23b083066444dca1e531396ae
Abhinav Manda <155139627+Abhinavmanda1857@users.noreply.github.com>
2025-03-07 17:42:15 +0000

    2523. Closest Prime Numbers in Range

---
+++ cpp/lc3/1/7/aa.cpp
@@ -0,0 +1,36 @@
+class Solution {
+    public:
+        vector<int> closestPrimes(int left, int right) {
+            vector<int> primes;
+            for (int i = left; i <= right; i++) {
+                if (i % 2 == 0 && i > 2) continue;
+                if (isPrime(i)) {
+                    if (!primes.empty() && i <= primes.back() + 2) {
+                        return {primes.back(), i};
+                    }
+                    primes.push_back(i);
+                }
+            }
+            if (primes.size() < 2) return {-1, -1};
+            vector<int> res(2);
+            int minDiff = INT_MAX;
+            for (int i = 1; i < primes.size(); i++) {
+                int diff = primes[i] - primes[i - 1];
+                if (diff < minDiff) {
+                    minDiff = diff;
+                    res[0] = primes[i - 1];
+                    res[1] = primes[i];
+                }
+            }
+            return res;
+        }
+    
+    private:
+        bool isPrime(int x) {
+            if (x == 1) return false;
+            for (int i = 3; i * i <= x; i += 2) {
+                if (x % i == 0) return false;
+            }
+            return true;
+        }
+    };
